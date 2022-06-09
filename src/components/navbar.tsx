d0044eae2ea74cd81fd994771a9f83904029a5eb
Robert <contentmediaguy@gmail.com>
2022-06-09 13:47:44 -0400

    finished page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,33 @@
 
 export default function NavBar() {
+    function handlePress() {
+        alert("Thanks for donating!")
+    }
+    
     return (
         <nav>
-            <ul className="nav-container">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Parks</a></li>
-                <li><a href="#">Work</a></li>
-                <li><a href="#">News</a></li>
-            </ul>
+            <div className="nav-container">
+                <ul className="mobile-menu">
+                    <li><a href="#home" className="isActive">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Parks</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <hr />
+            </div>
 
-            <button>Donate</button>
+            <div className="btn-container">
+                <div className="hamburger-menu">
+                    <div className="line line1"></div>
+                    <div className="line line2"></div>
+                    <div className="line line3"></div>
+                </div>
+
+                <button className="donate-btn" onClick={handlePress}>Donate</button>
+            </div>
+
+            <hr className="mobile-line" />
         </nav>
     )
 }
