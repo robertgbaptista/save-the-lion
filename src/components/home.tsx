32b420af350f6e6c9d41da8347106cda21aa5040
Robert <contentmediaguy@gmail.com>
2022-06-08 20:36:08 -0400

    Finished the Home component

---
+++ src/components/home.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <section id="home">
+            <div className="container">
+                <h1 className="home-title">Save the Lions</h1>
+                <h2 className="home-text">Protecting wildlife from poaching and other illegal activity is one of our top priorities.</h2>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul className="nav-container">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Parks</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+
+            <button>Donate</button>
+        </nav>
+    )
+}
